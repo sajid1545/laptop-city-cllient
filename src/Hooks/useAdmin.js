147b107672612785e9e5816006c34b95302f77c8
sajid9962 <clashking1545@gmail.com>
2022-11-24 04:34:24 +0600

    hooks created to check whether user is admin or seller

---
+++ src/Hooks/useAdmin.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+const useAdmin = (email) => {
+	const [isAdmin, setIsAdmin] = useState(false);
+	const [isAdminLoading, setIsAdminLoading] = useState(true);
+
+	fetch(`http://localhost:5000/user/seller/${email}`)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+			setIsAdmin(data.isAdmin);
+			setIsAdminLoading(false);
+		});
+
+	return [isAdmin, isAdminLoading];
+};
+
+export default useAdmin;
--- src/Hooks/useSeller.js
+++ src/Hooks/useSeller.js
@@ -2,6 +2,17 @@ import { useState } from 'react';
 
 const useSeller = (email) => {
 	const [isSeller, setIsSeller] = useState(false);
-	const [isSellerLoading, setIsSellerLoading] = useState(false);
-    
+	const [isSellerLoading, setIsSellerLoading] = useState(true);
+
+	fetch(`http://localhost:5000/user/seller/${email}`)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+			setIsSeller(data.isSeller);
+			setIsSellerLoading(false);
+		});
+
+	return [isSeller, isSellerLoading];
 };
+
+export default useSeller;
