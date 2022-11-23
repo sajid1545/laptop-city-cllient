284192a4051a85b33923cd147fc3f86b7727339e
sajid9962 <clashking1545@gmail.com>
2022-11-23 22:32:48 +0600

    laptop city info section added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HomeBanner from '../HomeBanner/HomeBanner';
+import LaptopCityInfo from '../LaptopCityInfo/LaptopCityInfo';
 
 const Home = () => {
 	return (
 		<div>
 			<HomeBanner />
+			<LaptopCityInfo />
 		</div>
 	);
 };
---
+++ src/Pages/Home/LaptopCityInfo/LaptopCityInfo.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import laptopInfoBanner from '../../../Assets/Images/laptopCityInfo.jpg';
+
+const LaptopCityInfo = () => {
+	return (
+		<div className="hero my-10">
+			<div className="hero-content flex-col lg:flex-row ">
+				<img src={laptopInfoBanner} className="rounded-lg lg:w-1/2 shadow-2xl" alt="" />
+				<div className="lg:w-1/2">
+					<p className="py-6">
+						Laptop City processes 1,000s of laptops per month. We are proud of our emphasis on high
+						quality, tested and working products. Our business thrives on the basis of these
+						principles of integrity, as we want our customers to be pleased with our products. We
+						understand that our success depends on your repeat business.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default LaptopCityInfo;
