3cc39314e13a40d39f16c4b42120a545eba075cf
sajid9962 <clashking1545@gmail.com>
2022-11-23 22:02:34 +0600

    home banner added

---
+++ src/Pages/Home/HomeBanner/HomeBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomeBanner.css';
+
+const HomeBanner = () => {
+	return (
+		<div className='banner text-5xl my-5 '>
+			<h1 className='font-bold'>Laptop City is a worldwide wholesaler and exporter of refurbished and used laptops .</h1>
+		</div>
+	);
+};
+
+export default HomeBanner;
