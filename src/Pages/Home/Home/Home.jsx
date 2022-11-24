42ecf454bdf44f485e3b8b9dc8e31a34477c1d15
sajid9962 <clashking1545@gmail.com>
2022-11-25 01:38:27 +0600

    slider added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,13 +1,31 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import Categories from '../Categories/Categories';
 import HomeBanner from '../HomeBanner/HomeBanner';
 import LaptopCityInfo from '../LaptopCityInfo/LaptopCityInfo';
+import Slider from '../Slider/Slider';
 
 const Home = () => {
+
+	const { data: advertisements = [], isLoading } = useQuery({
+		queryKey: [],
+		queryFn: () =>
+			fetch('http://localhost:5000/display-home-product', {
+				headers: {
+					authorization: `Bearer ${localStorage.getItem('laptop-city-token')}`,
+				},
+			}).then((res) => res.json()),
+	});
+
+
+	
+
 	return (
 		<div>
 			<HomeBanner />
 			<LaptopCityInfo />
+			
+			<Slider advertisements={advertisements} isLoading={ isLoading} />
 			<Categories />
 		</div>
 	);
---
+++ src/Pages/Home/Slider/Slider.jsx
@@ -0,0 +1,43 @@
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+import 'react-awesome-slider/dist/styles.css';
+import LargeSpinner from '../../Shared/Spinners/LargeSpinner';
+
+const Slider = ({ advertisements, isLoading }) => {
+	if (isLoading) {
+		return <LargeSpinner />;
+	}
+
+	const AutoplaySlider = withAutoplay(AwesomeSlider);
+	return (
+        <div>
+            
+			<div className="p-0 md:p-28  rounded mx-auto">
+				<AutoplaySlider
+					animation="cubeAnimation"
+					play={true}
+					cancelOnInteraction={false}
+					interval={5000}>
+					{advertisements.map(
+						(advertisement) =>
+							advertisement.productStatus &&
+							!advertisement.paid && (
+								<div key={advertisement._id} className="text-white space-y-5 text-center">
+									<h1 className="text-5xl font-extrabold text-white ">
+										{advertisement.productsName}
+									</h1>
+									<h2>{advertisement.description}</h2>
+									<img src={advertisement.picture} alt="" className="block mx-auto rounded-full" />
+								</div>
+							)
+					)}
+				</AutoplaySlider>
+			</div>
+		</div>
+	);
+};
+
+export default Slider;
