5b87c6febaf48be4ca9854f0a5f060b2e0eb97ee
sajid9962 <clashking1545@gmail.com>
2022-11-25 01:42:32 +0600

    slider bug fixed

--- src/Pages/Home/Slider/Slider.jsx
+++ src/Pages/Home/Slider/Slider.jsx
@@ -13,29 +13,34 @@ const Slider = ({ advertisements, isLoading }) => {
 
 	const AutoplaySlider = withAutoplay(AwesomeSlider);
 	return (
-        <div>
-            
-			<div className="p-0 md:p-28  rounded mx-auto">
-				<AutoplaySlider
-					animation="cubeAnimation"
-					play={true}
-					cancelOnInteraction={false}
-					interval={5000}>
-					{advertisements.map(
-						(advertisement) =>
-							advertisement.productStatus &&
-							!advertisement.paid && (
-								<div key={advertisement._id} className="text-white space-y-5 text-center">
-									<h1 className="text-5xl font-extrabold text-white ">
-										{advertisement.productsName}
-									</h1>
-									<h2>{advertisement.description}</h2>
-									<img src={advertisement.picture} alt="" className="block mx-auto rounded-full" />
-								</div>
-							)
-					)}
-				</AutoplaySlider>
-			</div>
+		<div>
+			{advertisements.length !== 0 && (
+				<div className="p-0 md:p-28  rounded mx-auto">
+					<AutoplaySlider
+						animation="cubeAnimation"
+						play={true}
+						cancelOnInteraction={false}
+						interval={5000}>
+						{advertisements.map(
+							(advertisement) =>
+								advertisement.productStatus &&
+								!advertisement.paid && (
+									<div key={advertisement._id} className="text-white space-y-5 text-center">
+										<h1 className="text-5xl font-extrabold text-white ">
+											{advertisement.productsName}
+										</h1>
+										<h2>{advertisement.description}</h2>
+										<img
+											src={advertisement.picture}
+											alt=""
+											className="block mx-auto rounded-full"
+										/>
+									</div>
+								)
+						)}
+					</AutoplaySlider>
+				</div>
+			)}
 		</div>
 	);
 };
