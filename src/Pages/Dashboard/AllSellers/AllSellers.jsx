9adbb124f4063d12baf84e1388afd2528412cca0
sajid9962 <clashking1545@gmail.com>
2022-11-25 12:18:06 +0600

    users can now be verified by admin

--- src/Pages/Dashboard/AllSellers/AllSellers.jsx
+++ src/Pages/Dashboard/AllSellers/AllSellers.jsx
@@ -1,9 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import LargeSpinner from '../../Shared/Spinners/LargeSpinner';
+import { toast } from 'react-hot-toast';
 
 const AllSellers = () => {
-	const { data: allSellers = [], isLoading } = useQuery({
+	const {
+		data: allSellers = [],
+		isLoading,
+		refetch,
+	} = useQuery({
 		queryKey: ['allSellers'],
 		queryFn: () =>
 			fetch('http://localhost:5000/all-sellers', {
@@ -17,6 +22,23 @@ const AllSellers = () => {
 		return <LargeSpinner />;
 	}
 
+	// verify user
+	const handleVerifyUser = (user) => {
+		console.log(user);
+		fetch(`http://localhost:5000/verify-user/${user._id}`, {
+			method: 'PUT',
+			headers: {
+				authorization: `Bearer ${localStorage.getItem('laptop-city-token')}`,
+			},
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				refetch();
+				toast.success(`${user.name} verified successfully`);
+			});
+	};
+
 	return (
 		<div>
 			<h1 className="text-4xl text-center font-bold">All Sellers</h1>
@@ -26,17 +48,36 @@ const AllSellers = () => {
 						<tr>
 							<th></th>
 							<th>Name</th>
-							<th>Job</th>
-							<th>Favorite Color</th>
+							<th>Email</th>
+							<th>Verify Seller</th>
+							<th>Action</th>
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<th>1</th>
-							<td>Cy Ganderton</td>
-							<td>Quality Control Specialist</td>
-							<td>Blue</td>
-						</tr>
+						{allSellers.map((seller, i) => (
+							<tr key={seller._id}>
+								<th>{i + 1}</th>
+								<td>{seller.name}</td>
+								<td>{seller.email}</td>
+								<td>
+									{!seller.verified && (
+										<button
+											onClick={() => handleVerifyUser(seller)}
+											className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+											Verify
+										</button>
+									)}
+
+									{seller.verified && <p className="text-xl font-bold text-green-700">Verified</p>}
+								</td>
+
+								<td>
+									<button className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:ring focus:ring-red-300 focus:ring-opacity-80">
+										Delete
+									</button>
+								</td>
+							</tr>
+						))}
 					</tbody>
 				</table>
 			</div>
